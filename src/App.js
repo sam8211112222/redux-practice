466f526221d6858d9f8a2adab60e2d93049825fb
Sam Chen <sam8211112222@gmail.com>
2023-01-07 17:02:22 +0800

    Add React-Redux

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
+import CarForm from "./components/CarForm";
+import CarList from "./components/CarList";
+import CarSearch from "./components/CarSearch";
+import CarValue from "./components/CarValue";
+
 function App() {
   return (
     <div>
