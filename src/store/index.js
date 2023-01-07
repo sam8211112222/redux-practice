3a7ef9f26d45ba8dde6516e41b03d9bb621c96bc
Sam Chen <sam8211112222@gmail.com>
2023-01-07 16:59:33 +0800

    Add CarSlice

---
+++ src/store/index.js
@@ -0,0 +1,17 @@
+import { configureStore } from '@reduxjs/toolkit';
+import {
+    carsReducer,
+    addCar,
+    removeCar,
+    changeSearchTerm,
+} from './slices/carsSlice';
+import { formReducer, changeName, changeCost } from './slices/formsSlice';
+
+const store = configureStore({
+    reducer: {
+        cars: carsReducer,
+        form: formReducer,
+    },
+});
+
+export { store, changeName, changeCost, addCar, removeCar, changeSearchTerm };
